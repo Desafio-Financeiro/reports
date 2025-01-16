116f4635d154df89c90e6ff03f806451ae8638c3
IsabellaSoares <isabella.soares.28@gmail.com>
2025-01-15 22:55:40 -0300

    feat: adjust app to module federation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
-import { Component } from '@angular/core';
-import { ChartModule } from 'primeng/chart';
-import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
-import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { Component } from "@angular/core";
+
 @Component({
-  selector: 'app-root',
-  imports: [ChartModule, DoughnutChartComponent, BarChartComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { Injector, NgModule } from "@angular/core";
+import { ChartModule } from "primeng/chart";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppComponent } from "./app.component";
+import { createCustomElement } from "@angular/elements";
+import { DoughnutChartModule } from "./doughnut-chart/doughnut-chart.module";
+import { BarChartModule } from "./bar-chart/bar-chart.module";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, ChartModule, DoughnutChartModule, BarChartModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+  constructor(private injector: Injector) {
+    const element = createCustomElement(AppComponent, { injector: injector });
+    customElements.define("angular-app-component", element);
+  }
+}
