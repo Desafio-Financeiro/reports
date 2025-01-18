1857a4edbc9a22b083c34dfab1d1c1ebce21b781
IsabellaSoares <isabella.soares.28@gmail.com>
2025-01-17 22:35:16 -0300

    feat: implements api service

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  getBalance() {
+    return this.http.get('http://localhost:8080/balance/1').pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+}
